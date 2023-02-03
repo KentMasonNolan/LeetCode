6d527fa99cefecfdbd100321cfe0474b768660b5
Kent Nolan <kent.nolan@gmail.com>
2023-02-03 13:32:07 +1300

    Just ended up being a list of IF statements which will work once you account for I allowing to check for i+1 when the condition is build in.
    Next time I will use 'I': 1, 'V': 5, etc and change IV to IIII.
    
    Onto the next one.

--- Easy/romanToInt.py
+++ Easy/romanToInt.py
@@ -1,11 +1,28 @@
-s = 'IVI'
+s = 'III'
 
 def romanToInt(s):
     romanInt = 0
     romanArray = list(s)
-    length = len(romanArray)
 
-    for i in range(length):
+    for i in range(len(romanArray)):
+        if romanArray[i] == 'I' and romanArray[i+1] == 'V':
+            romanInt += 4
+            break
+        if romanArray[i] == 'I' and romanArray[i+1] == 'X':
+            romanInt += 9
+            break
+        if romanArray[i] == 'X' and romanArray[i+1] == 'L':
+            romanInt += 40
+            i += 1
+        if romanArray[i] == 'X' and romanArray[i+1] == 'C':
+            romanInt += 90
+            i += 1
+        if romanArray[i] == 'C' and romanArray[i+1] == 'D':
+            romanInt += 400
+            i += 1
+        if romanArray[i] == 'C' and romanArray[i+1] == 'M':
+            romanInt += 900
+            i += 1
         if romanArray[i] == 'I':
             romanInt += 1
         if romanArray[i] == 'V':
@@ -14,6 +31,12 @@ def romanToInt(s):
             romanInt += 10
         if romanArray[i] == 'L':
             romanInt += 50
+        if romanArray[i] == 'C':
+            romanInt += 100
+        if romanArray[i] == 'D':
+            romanInt += 500
+        if romanArray[i] == 'M':
+            romanInt += 1000
 
     print(romanInt)
 
