09948870d224a2fc9a6d9cbca8afb58fc4d6f950
Kent Nolan <kent.nolan@gmail.com>
2023-09-11 19:59:57 +1200

    Unsure the update.

--- Easy/Palindrome.py
+++ Easy/Palindrome.py
@@ -1,23 +1,22 @@
 import math
 
-x = int(input("Enter your value: "))
 
-def isPalindrome(x):
-    halfValueLength = math.trunc(len(str(x))/2)
-    i = 0
-    j = len(str(x)) - 1
-    x_array = [int(t) for t in str(x)]
-    if x <= 0:
-        print("False")
-        return False
-    while i <= halfValueLength:
-        if x_array[i] != x_array[j]:
+class Solution(object):
+    def isPalindrome(self, s):
+        halfValueLength = math.trunc(len(str(s))/2)
+        i = 0
+        j = len(str(s)) - 1
+        x_array = [int(t) for t in str(s)]
+        if s <= 0:
             print("False")
             return False
-        else:
-            i += 1
-            j -= 1
-    print("True")
-    return True
+        while i <= halfValueLength:
+            if x_array[i] != x_array[j]:
+                print("False")
+                return False
+            else:
+                i += 1
+                j -= 1
+        print("True")
+        return True
 
-isPalindrome(x)
